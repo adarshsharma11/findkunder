187c34dda091505cbf5e8d42de608cb82d52652b
adarshsharma11 <adarshsharma002@gmail.com>
2024-06-20 17:34:31 +0530

    lead details page updates

--- resources/js/App/src/app/main/pages/leads/lead-details/components/LeadDetails.jsx
+++ resources/js/App/src/app/main/pages/leads/lead-details/components/LeadDetails.jsx
@@ -33,20 +33,13 @@ function LeadDetails(props) {
         <div className="md:flex">
           <div className="flex flex-col flex-1 md:ltr:pr-32 md:rtl:pl-32">
             <Card component={motion.div} variants={item} className="w-full mb-32">
-              <div className="px-32 pt-24">
-                <Typography className="text-2xl font-semibold leading-tight">
-                  General Information
-                </Typography>
-              </div>
-
               <CardContent className="px-32 py-24">
                 <Grid container spacing={3}>
-                  <Grid item xs={12} md={6}>
-                    <div className="mb-24">
-                      <Typography className="font-semibold mb-4 text-15">
-                        Company Name
-                      </Typography>
-                      <Typography>{data.company_name}</Typography>
+                  <Grid item xs={12} md={4}>
+                  <div className="mb-24">
+                    <Typography className="text-2xl font-semibold leading-tight">
+                    CONTACT
+                    </Typography>
                     </div>
                     <div className="mb-24">
                       <Typography className="font-semibold mb-4 text-15">
@@ -66,17 +59,42 @@ function LeadDetails(props) {
                       </Typography>
                       <Typography>{data.contact_phone}</Typography>
                     </div>
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                  <div className="mb-24">
+                    <Typography className="text-2xl font-semibold leading-tight">
+                    COMPANY
+                    </Typography>
+                    </div>
+                    <div className="mb-24">
+                      <Typography className="font-semibold mb-4 text-15">
+                        Company Name
+                      </Typography>
+                      <Typography>{data.company_name}</Typography>
+                    </div>
+                    <div className="mb-24">
+                      <Typography className="font-semibold mb-4 text-15">
+                      Tell us a bit about your company
+                      </Typography>
+                      <Typography>{data.company_description || "N/A"}</Typography>
+                    </div>
+                    <div className="mb-24">
+                      <Typography className="font-semibold mb-4 text-15">
+                        Website
+                      </Typography>
+                      <Typography>{data.website || "N/A"}</Typography>
+                    </div>
                     <div className="mb-24">
                       <Typography className="font-semibold mb-4 text-15">
                         CVR Number
                       </Typography>
-                      <Typography>{data.cvr_number}</Typography>
+                      <Typography>{data.cvr_number || "N/A"}</Typography>
                     </div>
                     <div className="mb-24">
                       <Typography className="font-semibold mb-4 text-15">
-                        City
+                       Who are you
                       </Typography>
-                      <Typography>{data.city}</Typography>
+                      <Typography>{data.customer_type.name}</Typography>
                     </div>
                     <div className="mb-24">
                       <Typography className="font-semibold mb-4 text-15">
@@ -90,17 +108,49 @@ function LeadDetails(props) {
                       </Typography>
                       <Typography>{data.postal_code}</Typography>
                     </div>
-                  </Grid>
-                  <Grid item xs={12} md={6}>
                     <div className="mb-24">
                       <Typography className="font-semibold mb-4 text-15">
-                        Customer Type
+                        City
                       </Typography>
-                      <Typography>{data.customer_type.name}</Typography>
+                      <Typography>{data.city}</Typography>
                     </div>
                     <div className="mb-24">
                       <Typography className="font-semibold mb-4 text-15">
-                        Physical Attendance Required
+                       Your location
+                      </Typography>
+                      <Typography>{data.location.name || "N/A"}</Typography>
+                    </div>  
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                  <div className="mb-24">
+                    <Typography className="text-2xl font-semibold leading-tight">
+                    ADDITIONAL INFORMATION
+                    </Typography>
+                    </div>
+                    <div className="mb-24">
+                      <Typography className="font-semibold mb-4 text-15">
+                        Who Do You Need
+                      </Typography>
+                      <Typography>{data.who_do_you_need || "N/A"}</Typography>
+                    </div>
+                    <div className="mb-24">
+                      <Typography className="font-semibold mb-4 text-15">
+                      What do you need help for
+                      </Typography>
+                      {data.categories.length > 0 && data.categories.map((category) => (
+                        <Typography key={category.id}>{category.name}</Typography>
+                      ))}
+                      {data.categories.length === 0 && <Typography>N/A</Typography>}
+                    </div>
+                    <div className="mb-24">
+                      <Typography className="font-semibold mb-4 text-15">
+                      Do you have any specific preferences
+                      </Typography>
+                      <Typography>{data.specific_preferences || "N/A"}</Typography>
+                    </div>
+                    <div className="mb-24">
+                      <Typography className="font-semibold mb-4 text-15">
+                      Is physical attendance required
                       </Typography>
                       <Typography>{data.physical_attendance_required || "N/A"}</Typography>
                     </div>
@@ -112,35 +162,9 @@ function LeadDetails(props) {
                     </div>
                     <div className="mb-24">
                       <Typography className="font-semibold mb-4 text-15">
-                        Specific Preferences
+                      Are there any bookkeepers/accountant we should not contact
                       </Typography>
-                      <Typography>{data.specific_preferences || "N/A"}</Typography>
-                    </div>
-                    <div className="mb-24">
-                      <Typography className="font-semibold mb-4 text-15">
-                        Status
-                      </Typography>
-                      <PriorityStatus priority={data.status} isDetailsPage />
-                    </div>
-                    <div className="mb-24">
-                      <Typography className="font-semibold mb-4 text-15">
-                        Website
-                      </Typography>
-                      <Typography>{data.website || "N/A"}</Typography>
-                    </div>
-                    <div className="mb-24">
-                      <Typography className="font-semibold mb-4 text-15">
-                        Who Do You Need
-                      </Typography>
-                      <Typography>{data.who_do_you_need || "N/A"}</Typography>
-                    </div>
-                    <div className="mb-24">
-                      <Typography className="font-semibold mb-4 text-15">
-                        Categories
-                      </Typography>
-                      {data.categories.map((category) => (
-                        <Typography key={category.id}>{category.name}</Typography>
-                      ))}
+                      <Typography>{data.do_not_contact || "N/A"}</Typography>
                     </div>
                   </Grid>
                 </Grid>
