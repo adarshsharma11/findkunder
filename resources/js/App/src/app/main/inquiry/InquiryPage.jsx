595f11f9a9c3468264f6d091b6a745494ba31362
adarshsharma11 <adarshsharma002@gmail.com>
2024-08-21 16:36:20 +0530

    react helmet for pages meta tags

--- resources/js/App/src/app/main/inquiry/InquiryPage.jsx
+++ resources/js/App/src/app/main/inquiry/InquiryPage.jsx
@@ -1,4 +1,5 @@
 import _ from "@lodash";
+import { Helmet } from 'react-helmet';
 import Paper from "@mui/material/Paper";
 import HeroBox from "../../shared-components/HeroBox";
 import InquiryStepForm from "./InquiryStepForm";
@@ -10,6 +11,10 @@ function InquiryPage() {
 
   return (
     <div className="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-1 min-w-0">
+      <Helmet>
+        <title>Inquiry - Find Bookkeeper or Accountant</title>
+        <meta name="description" content="Fill out our form and we'll connect you with top freelance bookkeepers and accountants, all for free and with no obligation." />
+      </Helmet>
       <Paper className="h-full sm:h-auto md:flex md:items-center md:justify-end w-full sm:w-auto md:h-full md:w-1/2 py-8 px-16 sm:p-48 md:p-64 sm:rounded-2xl md:rounded-none sm:shadow md:shadow-none ltr:border-r-1 rtl:border-l-1">
         <div className="w-full">
             <InquiryStepForm />
