8e5a14806b98ae9e7388a86a0a3f39544b10ad0a
adarshsharma11 <adarshsharma002@gmail.com>
2025-01-15 11:06:09 +0530

    owner role updates

---
+++ database/migrations/2025_01_15_052121_add_owner_role.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            Role::firstOrCreate(['name' => 'owner']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            Role::where('name', 'owner')->delete();
+        });
+    }
+};
