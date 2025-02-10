35f9d2519f2084bb621b4e8550523f90d935a8ba
adarshsharma11 <adarshsharma002@gmail.com>
2025-02-10 14:15:17 +0530

    add location button

--- resources/js/App/src/app/main/pages/companies/company-details/tabs/LocationInfoTab.jsx
+++ resources/js/App/src/app/main/pages/companies/company-details/tabs/LocationInfoTab.jsx
@@ -1,9 +1,35 @@
+import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 import LocationsTable from "./LocationTable";
 
 function LocationInfoTab(props) {
   const { userId, productId } = props;
+  const navigate = useNavigate();
   return (
     <div>
+       <div className="flex justify-end mb-16">
+        <Button
+        className="whitespace-nowrap"
+        variant="contained"
+        color="primary"
+        size="small"
+        onClick={() => {
+          if (productId !== 'new') {
+            navigate(`/locations/new/${productId}`)
+          } else {
+            navigate('/locations/new')
+          }
+        }}
+        startIcon={
+            <FuseSvgIcon size={20}>
+            heroicons-solid:external-link
+            </FuseSvgIcon>
+        }
+        >
+            Add New
+        </Button>
+       </div>
       <LocationsTable userId={userId} productId={productId} {...props} />
     </div>
   );
